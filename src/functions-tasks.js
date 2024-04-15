51831472cf602466847393c0912edb973d45b11e
Natalia Belyatskaya <natbeliatsky@gmail.com>
2024-04-16 02:53:16 +0300

    feat: add 9 tasks solutions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -107,8 +107,19 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = new Map();
+  return (...args) => {
+    const key = args[0];
+    let res;
+    if (cache.has(key)) {
+      res = cache.get(key);
+    } else {
+      res = func(key);
+      cache.set(key, res);
+    }
+    return res;
+  };
 }
 
 /**
@@ -181,10 +192,9 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => fn.call(this, ...args1, ...args);
 }
-
 /**
  * Returns the id generator function that returns next integer starting
  * from specified number every time when invoking.
@@ -202,8 +212,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let start = startFrom;
+  return () => {
+    const current = start;
+    start += 1;
+    return current;
+  };
 }
 
 module.exports = {
